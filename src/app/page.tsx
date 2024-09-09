429ae10ab0eff6ad9e2fb96e06e05d55d3663b13
excitedmuck <33532101+excitedmuck@users.noreply.github.com>
2024-09-09 15:20:43 -0700

    fone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default function HomePage() {
         </div>
       </div>
       <footer className="text-center text-purple-200 py-4">
-        Made by Minor God of the Multiverse
+        Made by Minor God of the Multiverse.
       </footer>
     </main>
   );
